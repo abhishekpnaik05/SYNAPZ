469c9f13734a76e65855379cc4c06412b1d75b81
abhishekpnaik05 <abhishekpnaik05@gmail.com>
2025-07-20 09:39:44 +0530

    backend render

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -35,7 +35,7 @@ function Sidebar() {
         setCurrThreadId(newThreadId);
 
         try {
-            const response = await fetch(`http://localhost:8080/api/thread/${newThreadId}`);
+            const response = await fetch(`https://synapz-backend.onrender.com/api/thread/${newThreadId}`);
             const res = await response.json();
             console.log(res);
             setPrevChats(res);
@@ -48,7 +48,7 @@ function Sidebar() {
 
     const deleteThread = async (threadId) => {
         try {
-            const response = await fetch(`http://localhost:8080/api/thread/${threadId}`, {method: "DELETE"});
+            const response = await fetch(`https://synapz-backend.onrender.com/api/thread/${threadId}`, {method: "DELETE"});
             const res = await response.json();
             console.log(res);
 
